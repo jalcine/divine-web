cd4c19de13d6254fb8ecfd17c964c1ead111a96d
Jacky Alcine <yo@jacky.wtf>
2026-05-01 19:37:34 -0400

    move head back in place

--- src/App.tsx
+++ src/App.tsx
@@ -6,24 +6,28 @@
 // To add new routes, edit the AppRouter.tsx file.
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createHead, UnheadProvider } from '@unhead/react/client';
-import { InferSeoMetaPlugin } from '@unhead/addons';
-import { Suspense } from 'react';
-import NostrProvider from '@/components/NostrProvider';
-import { EventCachePreloader } from '@/components/EventCachePreloader';
-import { SentryUserSync } from '@/components/SentryUserSync';
-import { DivineJWTWindowNostr } from '@/components/DivineJWTWindowNostr';
+import { createHead, UnheadProvider } from "@unhead/react/client";
+import { InferSeoMetaPlugin } from "@unhead/addons";
+import { Suspense } from "react";
+import NostrProvider from "@/components/NostrProvider";
+import { EventCachePreloader } from "@/components/EventCachePreloader";
+import { SentryUserSync } from "@/components/SentryUserSync";
+import { DivineJWTWindowNostr } from "@/components/DivineJWTWindowNostr";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { NostrLoginProvider } from '@nostrify/react/login';
-import { AppProvider } from '@/components/AppProvider';
-import { NWCProvider } from '@/contexts/NWCContext';
-import { AppConfig } from '@/contexts/AppContext';
-import { VideoPlaybackProvider } from '@/contexts/VideoPlaybackContext';
-import { FullscreenFeedProvider } from '@/contexts/FullscreenFeedContext';
-import AppRouter from './AppRouter';
-import { PRIMARY_RELAY, PRESET_RELAYS, toLegacyFormat } from '@/config/relays';
+import { NostrLoginProvider } from "@nostrify/react/login";
+import { AppProvider } from "@/components/AppProvider";
+import { NWCProvider } from "@/contexts/NWCContext";
+import { AppConfig } from "@/contexts/AppContext";
+import { VideoPlaybackProvider } from "@/contexts/VideoPlaybackContext";
+import { FullscreenFeedProvider } from "@/contexts/FullscreenFeedContext";
+import AppRouter from "./AppRouter";
+import { PRIMARY_RELAY, PRESET_RELAYS, toLegacyFormat } from "@/config/relays";
+
+const head = createHead({
+  plugins: [InferSeoMetaPlugin()],
+});
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -35,18 +39,10 @@ const queryClient = new QueryClient({
   },
 });
 
-const head = createHead({
-  plugins: [
-    InferSeoMetaPlugin(),
-  ],
-});
-
 const defaultConfig: AppConfig = {
   theme: "system",
   relayUrl: PRIMARY_RELAY.url, // Primary relay with NIP-50 support
-  relayUrls: [
-    'wss://relay.divine.video',
-  ],
+  relayUrls: ["wss://relay.divine.video"],
 };
 
 const presetRelays = toLegacyFormat(PRESET_RELAYS);
@@ -54,9 +50,13 @@ const presetRelays = toLegacyFormat(PRESET_RELAYS);
 export function App() {
   return (
     <UnheadProvider head={head}>
-      <AppProvider storageKey="nostr:app-config" defaultConfig={defaultConfig} presetRelays={presetRelays}>
+      <AppProvider
+        storageKey="nostr:app-config"
+        defaultConfig={defaultConfig}
+        presetRelays={presetRelays}
+      >
         <QueryClientProvider client={queryClient}>
-          <NostrLoginProvider storageKey='nostr:login'>
+          <NostrLoginProvider storageKey="nostr:login">
             <NostrProvider>
               <DivineJWTWindowNostr />
               <EventCachePreloader />
